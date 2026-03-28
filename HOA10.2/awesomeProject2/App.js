efcc41c1468a20a88bcaed276a035e5542286862
Leon <qkl-pascual@tip.edu.ph>
2026-03-28 11:06:54 +0800

    edit: HOA10.2 files

--- HOA10.2/awesomeProject2/App.js
+++ HOA10.2/awesomeProject2/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Team Name</Text>
-      <Text>Ken Leonard Pascual</Text>
+      <Text style={styles.textStyle}>Team Name</Text>
+      <View>
+        <Text style={styles.memberBlue}>Ken Leonard Pascual</Text>
+        <Button title="Tap me!" color="green" />
+      </View>
+      <Text style={styles.memberRed}>Member 2</Text>
+      <Text style={styles.memberRed}>Member 3</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +22,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  member: {
+    margin: 16,
+    padding: 16,
+  },
+  memberBlue: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: 'blue',
+  },
+  memberRed: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: 'red',
+  },
+  textStyle: {
+    backgroundColor: 'blue',
+    color: 'white',
+    margin: 16,
+    padding: 16,
   },
 });
